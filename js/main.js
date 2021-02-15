67edec03370958a330d6980326f556126dde8f17
AlexNosevich <chivesonprojects@gmail.com>
2021-02-16 00:55:34 +0300

    готова секция main со слайдером, надо доработать кнопки

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+ 
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  
+  
+});
