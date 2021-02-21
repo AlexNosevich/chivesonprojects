3212492d4502744eb2526e4c74e674c346600203
AlexNosevich <chivesonprojects@gmail.com>
2021-02-21 10:54:01 +0300

    Десктоп готов на 100%

--- js/main.js
+++ js/main.js
@@ -16,3 +16,36 @@ const swiper = new Swiper('.swiper-container', {
   
 });
 new Splide( '.splide' ).mount();
+
+// Обработка формы
+  $(".form").each(function() {
+    $(this).validate({
+      errorClass: "invalid",
+      messages: {
+        name: {
+          required: "Введите Ваше имя",
+          minlength: "Имя не может быть короче 2-х символов",
+        },
+        email: {
+          required: "We need your email address to contact you",
+          email: "Your email address must be in the format of name@domain.com",
+        },
+        phone: {
+          required: "Введите ваш номер",
+          phone: "Ваш номер должен быть в формате +79999999999",
+          minlength: "Номер телефона не может быть короче 10 символов",
+
+        },
+      },
+    });
+  })
+  $('.phone').mask('+7 (000)-000-00-00');
+  $( ".phone" ).validate({
+    rules: {
+      field: {
+        required:"More that 10 symbols",
+        minlength: "More that 10 symbols",
+        
+      }
+    }
+  });
